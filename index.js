73fb4528fefbadd10f3359e5900e45f862740209
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-08-11 12:43:13 +0700

    add animation and mockup data

--- index.js
+++ index.js
@@ -1,1 +1,556 @@
-console.log("first");
+console.log("goooo");
+const ALL_GAMES = {
+  data: [
+    {
+      _id: "6437bb7eed09171bd99072c0",
+      appid: "730",
+      name: "Counter-Strike: Global Offensive",
+      release_date: "2012-08-21T00:00:00.000Z",
+      english: "1",
+      developer: ["Valve", "Hidden Path Entertainment"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "multi-player",
+        "steam achievements",
+        "full controller support",
+        "steam trading cards",
+        "steam workshop",
+        "in-app purchases",
+        "valve anti-cheat enabled",
+        "stats",
+      ],
+      genres: ["action", "free to play"],
+      steamspy_tags: ["fps", "multiplayer", "shooter"],
+      achievements: "167",
+      positive_ratings: 2644404,
+      negative_ratings: 402313,
+      average_playtime: 22494,
+      median_playtime: 6502,
+      owners: "50000000-100000000",
+      price: 0,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/730/header.jpg?t=1554409309",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/730/page_bg_generated_v6b.jpg?t=1554409309",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c1",
+      appid: "570",
+      name: "Dota 2",
+      release_date: "2013-07-09T00:00:00.000Z",
+      english: "1",
+      developer: ["Valve"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "multi-player",
+        "co-op",
+        "steam trading cards",
+        "steam workshop",
+        "steamvr collectibles",
+        "in-app purchases",
+        "valve anti-cheat enabled",
+      ],
+      genres: ["action", "free to play", "strategy"],
+      steamspy_tags: ["free to play", "moba", "strategy"],
+      achievements: "0",
+      positive_ratings: 863507,
+      negative_ratings: 142079,
+      average_playtime: 23944,
+      median_playtime: 801,
+      owners: "100000000-200000000",
+      price: 0,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/570/header.jpg?t=1554408611",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/570/page_bg_generated_v6b.jpg?t=1554408611",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c4",
+      appid: "304930",
+      name: "Unturned",
+      release_date: "2017-07-07T00:00:00.000Z",
+      english: "1",
+      developer: ["Smartly Dressed Games"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "single-player",
+        "online multi-player",
+        "online co-op",
+        "steam achievements",
+        "steam trading cards",
+        "vr support",
+        "steam workshop",
+        "in-app purchases",
+        "steam cloud",
+        "valve anti-cheat enabled",
+        "stats",
+        "includes level editor",
+      ],
+      genres: ["action", "adventure", "casual", "free to play", "indie"],
+      steamspy_tags: ["free to play", "survival", "zombies"],
+      achievements: "46",
+      positive_ratings: 292574,
+      negative_ratings: 31482,
+      average_playtime: 3248,
+      median_playtime: 413,
+      owners: "20000000-50000000",
+      price: 0,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/304930/header.jpg?t=1540101174",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/304930/page_bg_generated_v6b.jpg?t=1540101174",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c3",
+      appid: "4000",
+      name: "Garry's Mod",
+      release_date: "2006-11-29T00:00:00.000Z",
+      english: "1",
+      developer: ["Facepunch Studios"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "single-player",
+        "multi-player",
+        "co-op",
+        "cross-platform multiplayer",
+        "steam achievements",
+        "steam trading cards",
+        "captions available",
+        "steam workshop",
+        "steam cloud",
+        "valve anti-cheat enabled",
+        "includes level editor",
+      ],
+      genres: ["indie", "simulation"],
+      steamspy_tags: ["sandbox", "multiplayer", "funny"],
+      achievements: "29",
+      positive_ratings: 363721,
+      negative_ratings: 16433,
+      average_playtime: 12422,
+      median_playtime: 1875,
+      owners: "10000000-20000000",
+      price: 6.99,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/4000/header.jpg?t=1535986052",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/4000/page_bg_generated_v6b.jpg?t=1535986052",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c2",
+      appid: "440",
+      name: "Team Fortress 2",
+      release_date: "2007-10-10T00:00:00.000Z",
+      english: "1",
+      developer: ["Valve"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "multi-player",
+        "cross-platform multiplayer",
+        "steam achievements",
+        "steam trading cards",
+        "captions available",
+        "steam workshop",
+        "in-app purchases",
+        "partial controller support",
+        "valve anti-cheat enabled",
+        "stats",
+        "includes level editor",
+        "commentary available",
+      ],
+      genres: ["action", "free to play"],
+      steamspy_tags: ["free to play", "multiplayer", "fps"],
+      achievements: "520",
+      positive_ratings: 515879,
+      negative_ratings: 34036,
+      average_playtime: 8495,
+      median_playtime: 623,
+      owners: "20000000-50000000",
+      price: 0,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/440/header.jpg?t=1539304875",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/440/page_bg_generated_v6b.jpg?t=1539304875",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c5",
+      appid: "218620",
+      name: "PAYDAY 2",
+      release_date: "2013-08-13T00:00:00.000Z",
+      english: "1",
+      developer: ["OVERKILL - a Starbreeze Studio."],
+      platforms: ["windows", "linux"],
+      required_age: 18,
+      categories: [
+        "single-player",
+        "multi-player",
+        "co-op",
+        "online co-op",
+        "steam achievements",
+        "full controller support",
+        "steam trading cards",
+        "in-app purchases",
+      ],
+      genres: ["action", "rpg"],
+      steamspy_tags: ["co-op", "action", "fps"],
+      achievements: "1130",
+      positive_ratings: 308657,
+      negative_ratings: 56523,
+      average_playtime: 3975,
+      median_playtime: 890,
+      owners: "10000000-20000000",
+      price: 7.49,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/218620/header.jpg?t=1541531328",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/218620/page_bg_generated_v6b.jpg?t=1541531328",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c8",
+      appid: "72850",
+      name: "The Elder Scrolls V: Skyrim",
+      release_date: "2011-11-10T00:00:00.000Z",
+      english: "1",
+      developer: ["Bethesda Game Studios"],
+      platforms: ["windows"],
+      required_age: 16,
+      categories: [
+        "single-player",
+        "steam achievements",
+        "steam trading cards",
+        "steam workshop",
+        "partial controller support",
+        "steam cloud",
+      ],
+      genres: ["rpg"],
+      steamspy_tags: ["open world", "rpg", "fantasy"],
+      achievements: "75",
+      positive_ratings: 237303,
+      negative_ratings: 14951,
+      average_playtime: 7089,
+      median_playtime: 3885,
+      owners: "10000000-20000000",
+      price: 9.99,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/72850/header.jpg?t=1493767797",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/72850/page_bg_generated_v6b.jpg?t=1493767797",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c9",
+      appid: "252950",
+      name: "Rocket League®",
+      release_date: "2015-07-07T00:00:00.000Z",
+      english: "1",
+      developer: ["Psyonix, Inc."],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "single-player",
+        "multi-player",
+        "co-op",
+        "shared/split screen",
+        "cross-platform multiplayer",
+        "steam achievements",
+        "full controller support",
+        "steam trading cards",
+        "steam workshop",
+        "steam cloud",
+        "stats",
+      ],
+      genres: ["action", "indie", "racing", "sports"],
+      steamspy_tags: ["multiplayer", "racing", "soccer"],
+      achievements: "88",
+      positive_ratings: 242561,
+      negative_ratings: 21816,
+      average_playtime: 5825,
+      median_playtime: 1462,
+      owners: "5000000-10000000",
+      price: 14.99,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/252950/header_alt_assets_6.jpg?t=1556125255",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/252950/page_bg_generated_v6b.jpg?t=1556125255",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c6",
+      appid: "105600",
+      name: "Terraria",
+      release_date: "2011-05-16T00:00:00.000Z",
+      english: "1",
+      developer: ["Re-Logic"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "single-player",
+        "multi-player",
+        "online multi-player",
+        "co-op",
+        "online co-op",
+        "steam achievements",
+        "full controller support",
+        "steam trading cards",
+        "steam cloud",
+      ],
+      genres: ["action", "adventure", "indie", "rpg"],
+      steamspy_tags: ["sandbox", "adventure", "survival"],
+      achievements: "88",
+      positive_ratings: 255600,
+      negative_ratings: 7797,
+      average_playtime: 5585,
+      median_playtime: 1840,
+      owners: "5000000-10000000",
+      price: 6.99,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/105600/header.jpg?t=1548954652",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/105600/page_bg_generated_v6b.jpg?t=1548954652",
+      __v: 0,
+    },
+    {
+      _id: "6437bb7eed09171bd99072c7",
+      appid: "550",
+      name: "Left 4 Dead 2",
+      release_date: "2009-11-19T00:00:00.000Z",
+      english: "1",
+      developer: ["Valve"],
+      platforms: ["windows", "mac", "linux"],
+      required_age: 0,
+      categories: [
+        "single-player",
+        "multi-player",
+        "co-op",
+        "steam achievements",
+        "full controller support",
+        "steam trading cards",
+        "captions available",
+        "steam workshop",
+        "steam cloud",
+        "valve anti-cheat enabled",
+        "stats",
+        "includes source sdk",
+        "commentary available",
+      ],
+      genres: ["action"],
+      steamspy_tags: ["zombies", "co-op", "fps"],
+      achievements: "70",
+      positive_ratings: 251789,
+      negative_ratings: 8418,
+      average_playtime: 1615,
+      median_playtime: 566,
+      owners: "10000000-20000000",
+      price: 7.19,
+      header_image:
+        "https://steamcdn-a.akamaihd.net/steam/apps/550/header.jpg?t=1540300687",
+      background:
+        "https://steamcdn-a.akamaihd.net/steam/apps/550/page_bg_generated_v6b.jpg?t=1540300687",
+      __v: 0,
+    },
+  ],
+  page: 1,
+  total: 27069,
+};
+const ALL_GENRES = {
+  data: [
+    {
+      _id: "6437bb7eed09171bd99072bf",
+      name: "action",
+      count: 128579,
+      __v: 0,
+    },
+    {
+      _id: "6437bb86ed09171bd991463b",
+      name: "free to play",
+      count: 22041,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991463d",
+      name: "strategy",
+      count: 57028,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991463f",
+      name: "adventure",
+      count: 110199,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914641",
+      name: "indie",
+      count: 211345,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914643",
+      name: "rpg",
+      count: 48574,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914645",
+      name: "animation & modeling",
+      count: 791,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914647",
+      name: "video production",
+      count: 386,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914649",
+      name: "casual",
+      count: 107149,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991464b",
+      name: "simulation",
+      count: 58017,
+      __v: 0,
+    },
+  ],
+  page: 1,
+  total: 29,
+};
+
+// import * as mockupData from "/mockupData.js";
+const BASE_URL = "https://steam-api-mass.onrender.com";
+const init = () => {
+  //get 20 games to show on the main content
+  getGameList();
+  //render main content
+  //get 10 genres to show on the vertical nav-bar
+  //render vertical nav-bar
+};
+
+const getGameList = async (params) => {
+  try {
+    const { q, steamspy_tags, page, limit, genres } = { params };
+    let queryString = "";
+    if (q) {
+      queryString += `&q=${q}`;
+    }
+    if (steamspy_tags) {
+      queryString += `&steamspy_tags=${steamspy_tags}`;
+    }
+    if (page) {
+      queryString += `&page=${page}`;
+    }
+    if (limit) {
+      queryString += `&limit=${limit}`;
+    }
+    if (genres) {
+      queryString += `&genres=${genres}`;
+    }
+
+    const gamesList = await fetch(`${BASE_URL}/games${queryString}`);
+    console.log("gamesList", gamesList["data"]);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+};
+const getGenresList = async (params) => {
+  try {
+    const { page, limit } = { params };
+    let queryString = "";
+
+    if (page) {
+      queryString += `&page=${page}`;
+    }
+    if (limit) {
+      queryString += `&limit=${limit}`;
+    }
+
+    const genresList = await fetch(`${BASE_URL}/genres${queryString}`);
+    console.log("genresList", genresList["data"]);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+};
+const getTagsList = async (params) => {
+  try {
+    const { page, limit } = { params };
+    let queryString = "";
+
+    if (page) {
+      queryString += `&page=${page}`;
+    }
+    if (limit) {
+      queryString += `&limit=${limit}`;
+    }
+
+    const tagsList = await fetch(`${BASE_URL}/steamspy-tags${queryString}`);
+    console.log("tagsList", tagsList["data"]);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+};
+const getSingleGameDetail = async (params) => {
+  try {
+    const { appid } = { params };
+
+    const gameDetail = await fetch(`${BASE_URL}/single-game/${appid}`);
+    console.log("gameDetail", gameDetail["data"]);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+};
+
+/* render */
+const renderGenresList = () => {
+  const genresListElm = document.querySelector("#genres-list");
+  const ulGenresList = genresListElm.children[0];
+  ulGenresList.innerHTML = "";
+  //call api
+  const genresList = { ...ALL_GENRES }.data;
+  genresList.forEach((g) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<li>
+        <a href="#" class="nav-item">${g.name}</a>
+      </li>`;
+    ulGenresList.appendChild(li);
+  });
+  console.log(genresList);
+};
+const renderGamesList = () => {
+  const gamesListElm = document.querySelector("#games-list");
+  //   const ulGenresList = gamesListElm.children[0];
+  gamesListElm.innerHTML = "";
+  //call api
+  const gamesList = { ...ALL_GAMES }.data;
+  //
+  gamesList.forEach((g) => {
+    const col = document.createElement("div");
+    col.className = "col";
+    col.innerHTML = `<div class="item">
+      <img src=${g.header_image} alt="aaa" />
+      <div class="info">
+        <div class="detail">
+          <p>LOGO</p>
+          <h3>${g.name}</h3>
+        </div>
+      </div>
+  </div>`;
+    gamesListElm.appendChild(col);
+  });
+  console.log(gamesList);
+};
+// init();
+renderGenresList();
+renderGamesList();
