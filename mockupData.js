8664602a223f0610522780f813edf22f2fbb6ece
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-08-12 15:10:21 +0700

    update src

---
+++ mockupData.js
@@ -0,0 +1,66 @@
+const ALL_GENRES = {
+  data: [
+    {
+      _id: "6437bb7eed09171bd99072bf",
+      name: "action",
+      count: 128579,
+      __v: 0,
+    },
+    {
+      _id: "6437bb86ed09171bd991463b",
+      name: "free to play",
+      count: 22041,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991463d",
+      name: "strategy",
+      count: 57028,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991463f",
+      name: "adventure",
+      count: 110199,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914641",
+      name: "indie",
+      count: 211345,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914643",
+      name: "rpg",
+      count: 48574,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914645",
+      name: "animation & modeling",
+      count: 791,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914647",
+      name: "video production",
+      count: 386,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd9914649",
+      name: "casual",
+      count: 107149,
+      __v: 0,
+    },
+    {
+      _id: "6437bc4aed09171bd991464b",
+      name: "simulation",
+      count: 58017,
+      __v: 0,
+    },
+  ],
+  page: 1,
+  total: 29,
+};
